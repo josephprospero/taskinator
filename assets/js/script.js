a93aa1cc95c739e793edb70d8cb7d81d279a0ef6
Joseph Prospero <jprospero413@gmail.com>
2021-01-05 20:05:10 -0800

    finished lesson 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var taskFormHandler() = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    };
+
+    formEl.reset()
 
     var taskDataObj = {
         name : taskNameInput,
@@ -21,10 +28,10 @@ var createTaskEl = function(taskDataObj) {
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
 
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl);
 
     tasksToDoEl.appendChild(listItemEl);
 };
 
-formEl.addEventListener("submit", taskTaskHandler);
+formEl.addEventListener("submit", taskFormHandler);
